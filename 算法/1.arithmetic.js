d2a7b62e9435c8b8606befcd101871f8030fc179
kangling098 <461060819@qq.com>
2018-09-01 23:08:32 +0800

    新算法

--- 算法/1.arithmetic.js
+++ 算法/1.arithmetic.js
@@ -1,42 +1,25 @@
-// const floor_power_of2 = num => 2**Math.floor(Math.log2(num));
+// ### 20 2018年9月1日
+// 一个数组中有0,1,2三种数字，写一个针对这个数组的排序算法。 
 
+// 比如A=[0,1,2,0,1,1,2,1,1]排序完结果是[0,0,1,1,1,1,1,2,2]
 
-const floor_power_of2 = num => num<=0 ? NaN : 2**Math.floor(Number.isInteger(Math.log2(num)) ? Math.log2(num) - 1: Math.log2(num));
-console.log(floor_power_of2(64))
-console.log(floor_power_of2(63))
-console.log(floor_power_of2(33))
-console.log(floor_power_of2(32))
-console.log(floor_power_of2(0))
-console.log(floor_power_of2(0.5))
+// 要求：
+// 1. 尽可能节省内存空间
+// 2. 尽可能快
 
+// 其他：
 
-18. 2018年8月30日题目 答案
-写一个函数floor_power_of2，求比x小的最大二的整数次幂。
-例如
+// 请按照下述步骤，最后给出自己算法在本地执行的时间：
+// 1. 生成1亿个0-2之间的随机数字
+// 2. 对他们进行排序
+// 3. 统计排序的时间（不包括生成数字的时间），以毫秒计算。 
+// 4. 在`作业`最后标注一下自己的执行时间
 
-floor_power_of2(64) // 64
-floor_power_of2(63) // 32
-floor_power_of2(33) // 32
-floor_power_of2(32) // 32
-答案:
-
-function floor_power_of2(x){
-  x = x | (x >> 1)
-  x = x | (x >> 2)
-  x = x | (x >> 4)
-  x = x | (x >> 8)
-  x = x | (x >> 16)
-  x = x | (x >> 32)
-  return x - (x >> 1)
+// let arr = [];
+// for (let i= 0;i < 100000000; i++){
+//     arr.push(~~Math.random()*3)
+// }
+let A=[0,1,2,0,1,1,2,1,1];
+const sort = arr =>{
+    
 }
-
-参考:
-算法是要取x最高位，最左边的二进制位。 x | (x >> 1)相当于将所有1位置复制到它下一个位置： 0b101010 | (0b101010 >> 1 // 0b111111
-可以通过仔细分析x=0b101010和x=0b100000去理解这个算法，最终在return之前算法会得到0b11111……的二进制数
-答案2：
-
-function floor_power_of2(x) {
-  return 1 << Math.floor( Math.log2(x) )
-}
-
-// 目前的大部分PC答案2更快。 因为CPU支持log等数学指令。

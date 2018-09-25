50b7af423a0d4bdd87e911d80c48c0eab9aaff79
kangling098 <461060819@qq.com>
2018-09-25 18:53:13 +0800

    算法题更新

--- 算法/插入排序.js
+++ 算法/插入排序.js
@@ -2,18 +2,31 @@
 // 下面的排序
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 11, 10, 9];
 
-const insert = (A, i, x) => { 
-    let p = i - 1 // 设置循环不变式p,每次都是x和p做比较
-    while (p >= 0 && A[p] > x) {
-        A[p + 1] = A[p--];
-    }
-    A[p + 1] = x;
+// const insert = (A, i, x) => { 
+//     let p = i - 1 // 设置循环不变式p,每次都是x和p做比较
+//     while (p >= 0 && A[p] > x) {
+//         A[p + 1] = A[p--];
+//     }
+//     A[p + 1] = x;
 
+// }
+// const sort = A => { 
+//     for (let i = 1; i < A.length; i++) {
+//         insert(A, i, A[i]);
+//     }
+//     return A;
+// }
+// console.log(sort(arr))
+function insert(A,i,x){
+    let p = i-1;
+    if(p>=0&&A[p]>x){
+        A[p+1] = A[p--]
+    }
+    A[p+1] = x;
 }
-const sort = A => { 
-    for (let i = 1; i < A.length; i++) {
-        insert(A, i, A[i]);
+function sort(A){
+    for(let i = 1; i< A.length;i++){
+        insert(A,i,A[i])
     }
     return A;
 }
-console.log(sort(arr))
